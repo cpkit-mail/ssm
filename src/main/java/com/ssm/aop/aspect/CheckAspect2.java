f840cf534742d3d52f528e65acd0f5fa6c91a0d1
cpkit-mail <cpkit_mail@126.com>
2016-09-19 18:03:12 +0800

    aop

--- src/main/java/com/ssm/aop/aspect/CheckAspect2.java
+++ src/main/java/com/ssm/aop/aspect/CheckAspect2.java
@@ -3,19 +3,28 @@ package com.ssm.aop.aspect;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
- * 根据配置文件实现AOP实现登录功能
+ * 根据注解实现AOP实现登录功能
  *
  */
-public class CheckAspect {
+@Aspect
+public class CheckAspect2 {
 
-    private static final Logger log = LoggerFactory.getLogger(CheckAspect.class);
+    private static final Logger log = LoggerFactory.getLogger(CheckAspect2.class);
+
+    // @Pointcut注解指定了切点
+    @Pointcut("execution(* *.sleep())")
+    public void sleeppoint() {
+    }
 
     /**
      * 实现代码
@@ -24,7 +33,15 @@ public class CheckAspect {
      * @return
      * @throws Throwable
      */
-    public void beforeCheck(JoinPoint call) throws Throwable {
+    // 五种类型的通知：
+    // Before(前)  org.apringframework.aop.MethodBeforeAdvice
+    // After-returning(返回后) org.springframework.aop.AfterReturningAdvice
+    // After-throwing(抛出后) org.springframework.aop.ThrowsAdvice
+    // Arround(周围) org.aopaliance.intercept.MethodInterceptor
+    // Introduction(引入) org.springframework.aop.IntroductionInterceptor
+    // 指定了运行时的通知
+    @Around("sleeppoint()")
+    public Object aroundCheck(ProceedingJoinPoint call) throws Throwable {
 
         log.info("Aspect aroundCheck begin...");
 
@@ -48,8 +65,18 @@ public class CheckAspect {
 
             log.info("Interceptor：跳转到login页面！");
             request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
-            throw new Exception();
-        }
+
+            return false;
+        } else
+            return true;
+
+        // Object[] args = call.getArgs();
+        // if (args[0] == null || !(args[0] instanceof User)) {
+        // throw new AuthorizationException();
+        // }
+        // User user = (User) (args[0]);
+        //
+        // return user;
 
     }
 }
